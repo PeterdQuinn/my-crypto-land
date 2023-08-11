10a55dd7f70851c5795d24d00c313171445b2617
Peter Quinn <peterdquinnsr@gmail.com>
2023-08-11 15:05:53 -0700

    Cryptonest

--- app/page.js
+++ app/page.js
@@ -1,95 +1,71 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={styles.container}>
+      <HeroSection />
+      <FeaturesSection />
+      <TestimonialsSection />
+      <Footer />
+    </div>
+  );
+}
+
+function HeroSection() {
+  return (
+    <div className={styles.hero}>
+      <h1>Welcome to CryptoNest</h1>
+      <p>Your ultimate destination for crypto insights and tools.</p>
+      <button className={styles.cta}>Get Started</button>
+    </div>
+  );
+}
+
+function FeaturesSection() {
+  return (
+    <div className={styles.features}>
+      <div className={styles.feature}>
+        <h2>Crypto Tracking</h2>
+        <p>Track your favorite cryptocurrencies in real-time.</p>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.feature}>
+        <h2>Insights</h2>
+        <p>Get the latest market insights and trends.</p>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.feature}>
+        <h2>Trading Tools</h2>
+        <p>Access advanced tools for crypto trading.</p>
       </div>
-    </main>
-  )
+    </div>
+  );
+}
+
+function TestimonialsSection() {
+  return (
+    <div className={styles.testimonials}>
+      <div className={styles.testimonial}>
+        <p>"CryptoNest has revolutionized the way I view the crypto market."</p>
+        <span>- Alex Smith</span>
+      </div>
+      <div className={styles.testimonial}>
+        <p>"The insights provided here are unmatched!"</p>
+        <span>- Jamie Doe</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.links}>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms of Service</a>
+      </div>
+      <div className={styles.socials}>
+        {/* Add social media icons and links here */}
+      </div>
+    </div>
+  );
 }
