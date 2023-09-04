4f07721ccc3e767a49336851a8179f885a2ac018
Peter Quinn <peter@quickorganics.com>
2023-09-04 16:13:53 -0700

    New Page

--- app/page.js
+++ app/page.js
@@ -3,19 +3,6 @@ import styles from './page.module.css';
 
 export default function Page() {
   return (
-
-
-
-
-
-
-
-
-
-
-
-
-
     
     <div className={styles.container}>
       <header className={styles.header}>
